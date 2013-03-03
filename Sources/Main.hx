09280865540f7ecb627c00bb5f8245a28e9bfe86
Robert Konrad <robert@ktx-software.com>
2013-03-03 18:12:21 +0100

    Added a basic shader.

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,6 +4,7 @@ import kha.Starter;
 
 class Main {
 	public static function main() {
+		kha.Sys.needs3d = true;
 		new Starter().start(new Shader());
 	}
 }
--- Sources/Shader.hx
+++ Sources/Shader.hx
@@ -1,9 +1,45 @@
 package;
 
 import kha.Game;
+import kha.graphics.FragmentShader;
+import kha.graphics.VertexBuffer;
+import kha.graphics.VertexData;
+import kha.graphics.VertexShader;
+import kha.graphics.VertexStructure;
+import kha.graphics.VertexType;
+import kha.Loader;
+import kha.Painter;
 
 class Shader extends Game {
+	private var vertexShader: VertexShader;
+	private var fragmentShader: FragmentShader;
+	private var vertices: VertexBuffer;
+	
 	public function new() {
 		super("Shader", false);
+	}
+	
+	override public function loadFinished(): Void {
+		vertexShader = kha.Sys.graphics.createVertexShader(Loader.the.getShader("shader.vert").toString());
+		fragmentShader = kha.Sys.graphics.createFragmentShader(Loader.the.getShader("shader.frag").toString());
+		var structure = new VertexStructure();
+		structure.add("pos", VertexData.Float2, VertexType.Position);
+		vertices = kha.Sys.graphics.createVertexBuffer(3, structure);
+		var v = vertices.lock();
+		v[0] = -1;
+		v[1] = -1;
+		v[2] = 1;
+		v[3] = -1;
+		v[4] = -1;
+		v[5] = 1;
+		vertices.unlock();
+	}
+	
+	override public function render(painter: Painter): Void {
+		kha.Sys.graphics.setVertexShader(vertexShader);
+		kha.Sys.graphics.setFragmentShader(fragmentShader);
+		kha.Sys.graphics.linkShaders();
+		kha.Sys.graphics.setVertexBuffer(vertices);
+		kha.Sys.graphics.drawArrays();
 	}
 }
