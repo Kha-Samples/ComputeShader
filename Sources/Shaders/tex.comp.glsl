32216833fd2bf88ac7c4f80fed7cc1e6e4728f9d
Christian Reuter <christian@aoe-maniac.de>
2016-09-05 23:48:12 +0200

    More neutral shape for the texture. ;-)

--- Sources/Shaders/tex.comp.glsl
+++ Sources/Shaders/tex.comp.glsl
@@ -10,7 +10,8 @@ void main() {
 	vec2 halfSize = vec2(gl_NumWorkGroups.xy) / 2;
 
 	vec2 pos = (ivec2(gl_GlobalInvocationID.xy) - halfSize) * 1.5 / (halfSize.x * sine);
-	float col = (1 + sign(pow(pos.x, 2) * pow(pos.y, 3) - pow(pow(pos.x, 2) + pow(pos.y, 2) - 1, 3))) / 2;
+	//float col = (1 + sign(pow(pos.x, 2) * pow(pos.y, 3) - pow(pow(pos.x, 2) + pow(pos.y, 2) - 1, 3))) / 2;
+	float col = (1 - sign(pow(pos.x, 2) + pow(pos.y, 2) - 1)) / 2;
 
 	imageStore(destTex, storePos, vec4(col, 0.0, 0.0, 1.0));
 
